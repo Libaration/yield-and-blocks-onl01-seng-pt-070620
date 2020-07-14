f56082cc74e279b9c623d8196dd5dffdc00736ff
Cristian Deleon <libaration@gmail.com>
2020-07-14 04:26:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+<<<<<<< HEAD
   if block_given?
     i = 0
 
@@ -13,6 +14,16 @@ def hello_t(array)
   end
 end
 
+=======
+    i = 0
+    while i < array.length
+      yield array[i]
+    i = i + 1
+  end
+  array
+end
+
+>>>>>>> c9ffe3fd89106f69c363bfeb176a13aaac20012a
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
